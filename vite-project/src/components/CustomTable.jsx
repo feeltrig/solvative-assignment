a2c933a0c865761e4078cce83aa485ddf36da533
feeltrig <feeltrigmusic@gmail.com>
2024-06-03 13:33:45 -0700

    added components

---
+++ vite-project/src/components/CustomTable.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "../styles/customTable.scss";
+
+const CustomTable = () => {
+  return (
+    <div className="tableContainer">
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First</th>
+            <th>Last</th>
+            <th>Handle</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default CustomTable;
---
+++ vite-project/src/components/SearchBox.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import "../styles/searchBox.scss";
+
+const SearchBox = () => {
+  useEffect(() => {
+    window.addEventListener("keydown", function (e) {
+      console.log(e, e.ctrlKey && e.key == "/");
+      if (e.keyCode === 114 || (e.ctrlKey && e.key === "/")) {
+        if (document.getElementById("searchInput") !== document.activeElement) {
+          e.preventDefault();
+          console.log("Search is not in focus");
+          document.getElementById("searchInput").focus();
+        } else {
+          console.log("Default action of CtrlF");
+          return true;
+        }
+      }
+    });
+  }, []);
+
+  return (
+    <div className="searchBoxContainer">
+      <input
+        id="searchInput"
+        className="searchBox"
+        placeholder="Search places..."
+      />
+      <div className="keyboard-shortcut">Ctrl + /</div>
+    </div>
+  );
+};
+
+export default SearchBox;
